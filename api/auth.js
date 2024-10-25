700ca22533f7f11f730f4a781818d7b13f79b30d
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-25 11:53:05 -0300

    Criando api de login

---
+++ api/auth.js
@@ -0,0 +1,41 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+const { json } = require('body-parser')
+
+//retornar função 'app' para o consign carregar 
+//como os outros módulos
+module.exports = app => {
+    const signin = async (req, res) => {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400)
+                .send('Dados imcompletos')
+        }
+
+        const user = await app.db('users')
+            .where({ email: req.body.email })
+            .first()
+
+        if (user) {
+            bcrypt.compare(req.body.password,
+                user.password,
+                (err, isMatch) => {
+                    if(err || !isMatch) {
+                        return res.status(401).send('Dados inválidos')
+                    }
+                    //valor que será armazenado no token
+                    const payload = { id: user.id }
+
+                    res.json({
+                        name: user.name,
+                        email: user.email,
+                        token: jwt.encode(payload, authSecret)
+                    })
+                })
+        } else {
+            return res.status(400).send('Usuário não encontrado')
+        }
+    }
+
+    return { signin }
+}
